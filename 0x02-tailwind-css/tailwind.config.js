ed893cdd54201127758221ef1badfce7e5df621b
Derrick Mwangi <mwangiderrick640@gmail.com>
2025-05-20 02:39:36 +0000

    Modified the tailwind config file

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -1,6 +1,10 @@
 /* @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: ["./**/*.html","./src/**/*.{html,js}"],
+  content: [
+    "./*.html",      // This matches all HTML files in the root directory
+    "./src/**/*.html" // Just in case you put HTML in src later
+  ],
   theme: {
     extend: {},
   },
